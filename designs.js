a5ef89485e0ed4703ec2017d34f215b46e9ae1b5
Federico Boncaldo <federico@cryptolab.net>
2018-01-28 09:52:40 +0000

    Add script code to create the grid and draw

--- designs.js
+++ designs.js
@@ -3,8 +3,41 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+/**
+ * @description Create a grid of the weight and height specified
+ * @param event Event object
+ * @param weight weight of the table
+ * @param height height of the table
+ */
+function makeGrid(event, weight, height){
+	let row, column, tableRows;
 
-// Your code goes here!
+	event.preventDefault();
 
+	$('#pixelCanvas').empty();
+
+	for(row = 0; row < height; row++){
+		$('#pixelCanvas').append('<tr></tr>');
+		for(column = 0; column < weight; column++){
+			tableRows = $('tr');
+			$(tableRows[row]).append('<td></td>');
+		}
+	}
 }
+
+/**
+ * @description Fill the cell clicked with the color selected
+ */
+function draw(){
+	$('td').click(function(){
+		const color = $('#colorPicker').val();
+		$(this).css('background-color', color);
+	});
+}
+
+$('#sizePicker').submit(function (event) {
+	const weight = $('#inputWeight').val();
+	const height = $('#inputHeight').val();
+	makeGrid(event, weight, height);
+	draw();
+});
